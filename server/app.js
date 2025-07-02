e6c022039188baafabddc9a0501cfee1222bfb2b
Rsoniie <roshansoniee@gmail.com>
2025-07-02 09:19:31 +0530

    server setup initiated

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import Fastify from 'fastify';
+const fastify = Fastify({ logger: true });
+import connectToMongo from './connector/mongoConnect.js';
+import routes from './src/routes/routes.js';
+import config from 'config';
+
+fastify.register(routes);
+
+await connectToMongo();
+
+const port = config.get('port');
+const startFunc = async () => {
+    try {
+        await fastify.listen({ port: port });
+        console.log(`Server is running on port ${port}`);
+    } catch (error) {
+        console.error('Error starting server:', error);
+        process.exit(1);
+    }
+}
+
+startFunc();
---
+++ server/connector/mongoConnect.js
@@ -0,0 +1,19 @@
+import config from 'config';
+import mongoose, { mongo } from "mongoose";
+let mongoUri = config.get("db.development.mongoUri");
+
+const connectToMongo = async() => {
+    try {
+
+        console.log("This is mongoURI", mongoUri);
+        const connection = await mongoose.connect(mongoUri);
+        console.log("MongoDB connected successfully");
+        return connection;
+    } catch (error) {
+        console.error("Error in connecting to MongoDB", error);
+        throw error;
+    }
+}
+
+
+export default connectToMongo;
